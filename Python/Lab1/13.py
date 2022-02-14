ffcaa871a83ada3c24089dc505038610c2980377
MrKiller941 <sobolew.dmitriy2016@yandex.ru>
2022-02-14 21:55:16 +0700

    update ex.13

--- Python/Lab1/13.py
+++ Python/Lab1/13.py
@@ -2,6 +2,6 @@ a = int(input())
 b = int(input())
 s = input()
 print(s * a)
-for i in range(b - 2):
-    print(s, ' ' * (a - 2), s, sep='')
-print(s * a)
+for i in range(1,  a-1):
+    print(s + " " * (b-2) +s)
+print(s * b)
